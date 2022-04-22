c298a045e8abedb7900266cdaa793fc4b38db590
DimDimShishkov <d.shishkov.russia@gmail.com>
2022-04-22 16:22:02 +0300

    popup fixed

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,31 @@
 let editButton = document.querySelector('.profile__editButton');
 let addButton = document.querySelector('.profile__addButton');
-
+let page = document.querySelector('.page');
 let popup = document.querySelector('.popup');
 let submitButton = document.querySelector('.popup__submitButton');
 let exitButton = document.querySelector('.popup__exitButton');
 
-let like = document.querySelector('.element__like');
+function togglePopup() {
+    popup.classList.toggle('popup_opened');
+    page.classList.toggle('page_active');
+};
 
-editButton.addEventListener('click', function() {
-    popup.classList.toggle('popup_opened')
+editButton.addEventListener('click', function(event) {
+    togglePopup();
 });
 
-exitButton.addEventListener('click', function() {
-    popup.classList.toggle('popup_opened')
+popup.addEventListener('click', function(event) {
+    if(event.target == event.currentTarget){
+        popup.classList.remove('popup_opened');
+        page.classList.remove('page_active');
+    }
+});
+
+exitButton.addEventListener('click', function(event) {
+    togglePopup();
+});
+
+submitButton.addEventListener('click', function(event) {
+    togglePopup();
 });
 
