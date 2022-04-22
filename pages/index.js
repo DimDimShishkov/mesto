f86387ef3d47f4731f3c17d18e4bcd0bca60f477
DimDimShishkov <d.shishkov.russia@gmail.com>
2022-04-22 22:32:28 +0300

    changes in js file

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+let page = document.querySelector('.page');
+
 let editButton = document.querySelector('.profile__editButton');
 let addButton = document.querySelector('.profile__addButton');
-let page = document.querySelector('.page');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+
 let popup = document.querySelector('.popup');
 let submitButton = document.querySelector('.popup__submitButton');
 let exitButton = document.querySelector('.popup__exitButton');
+let profileNameEdit = document.querySelector('.popup__name');
+let profileDescriptionEdit = document.querySelector('.popup__description');
 
 function togglePopup() {
     popup.classList.toggle('popup_opened');
@@ -12,13 +18,6 @@ function togglePopup() {
 
 editButton.addEventListener('click', function(event) {
     togglePopup();
-});
-
-popup.addEventListener('click', function(event) {
-    if(event.target == event.currentTarget){
-        popup.classList.remove('popup_opened');
-        page.classList.remove('page_active');
-    }
 });
 
 exitButton.addEventListener('click', function(event) {
@@ -32,10 +31,9 @@ submitButton.addEventListener('click', function(event) {
     event.preventDefault();
 });
 
-let profileName = document.querySelector('.profile__name');
-let profileDescription = document.querySelector('.profile__description');
-
-let profileNameEdit = document.querySelector('.popup__name');
-let profileDescriptionEdit = document.querySelector('.popup__description');
-
-
+popup.addEventListener('click', function(event) {
+    if(event.target == event.currentTarget){
+        popup.classList.remove('popup_opened');
+        page.classList.remove('page_active');
+    }
+});
