3fe6d5397141fd63d6d518b7c898be29a52331f2
DimDimShishkov <d.shishkov.russia@gmail.com>
2022-05-08 14:55:22 +0300

    js: added popup for images and delete button

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,172 @@
-let page = document.querySelector('.page');
+const page = document.querySelector(".page");
 
-let profile = page.querySelector('.profile');
-let profileInfo = profile.querySelector('.profile__info');
-let editButton = profileInfo.querySelector('.profile__edit-button');
-let profileName = profileInfo.querySelector('.profile__name');
-let profileDescription = profileInfo.querySelector('.profile__description');
-let addButton = profile.querySelector('.profile__add-button');
+const profile = page.querySelector(".profile");
+const profileInfo = profile.querySelector(".profile__info");
+const editButton = profileInfo.querySelector(".profile__edit-button");
+const addImageButton = profile.querySelector(".profile__add-button");
+const profileName = profileInfo.querySelector(".profile__name");
+const profileDescription = profileInfo.querySelector(".profile__description");
+const addButton = profile.querySelector(".profile__add-button");
 
-let popup = page.querySelector('.popup');
-let popupContainer = popup.querySelector('.popup__container');
-let popupForm = popupContainer.querySelector('.popup__form');
-let submitButton = popupForm.querySelector('.popup__submit-button');
-let profileNameEdit = popupForm.querySelector('.popup__info_value_name');
-let profileDescriptionEdit = popupForm.querySelector('.popup__info_value_description');
-let exitButton = popupContainer.querySelector('.popup__exit-button');
+/* всплывающее окно для изменения имени профиля */
+const popupInformacion = page.querySelector(".popup_type_info");
+const popupContainerInfo = popupInformacion.querySelector(
+  ".popup__container_type_info"
+);
+const popupFormInfo = popupContainerInfo.querySelector(".popup__form_type_info");
+const submitButtonInfo = popupFormInfo.querySelector(
+  ".popup__submit-button_type_info"
+);
+const profileNameEdit = popupFormInfo.querySelector('[name="edit_name"]');
+const profileDescriptionEdit = popupFormInfo.querySelector(
+  '[name="edit_description"]'
+);
+const exitButtonInfo = popupContainerInfo.querySelector(
+  ".popup__exit-button_type_info"
+);
 
-function togglePopup() {
-    popup.classList.toggle('popup_opened');
-    page.classList.toggle('page_active');
+/* всплывающее окно для добавления картинок */
+const popupImages = page.querySelector(".popup_type_images");
+const popupContainerImages = popupImages.querySelector(
+  ".popup__container_type_images"
+);
+const popupFormImages = popupContainerImages.querySelector(
+  ".popup__form_type_images"
+);
+const submitButtonImages = popupFormImages.querySelector(
+  ".popup__submit-button_type_images"
+);
+const elementsTextEdit = popupFormImages.querySelector('[name="edit_text"]');
+const elementsImageEdit = popupFormImages.querySelector('[name="edit_image"]');
+const exitButtonImages = popupContainerImages.querySelector(
+  ".popup__exit-button_type_images"
+);
+
+const elements = page.querySelector(".elements");
+
+/* функция открытия попапа и блокировки страницы */
+const togglePopup = (popup) => {
+  popup.classList.toggle("popup_opened");
+  page.classList.toggle("page_active");
 };
 
-function formSubmitHandler() {
-    togglePopup();
-    profileName.textContent = profileNameEdit.value;
-    profileDescription.textContent = profileDescriptionEdit.value;
-    event.preventDefault();
+/* функция внесения информации с попапа инфо в блок профиля */
+const formSubmitHandlerInfo = () => {
+  togglePopup(popupInformacion);
+  profileName.textContent = profileNameEdit.value;
+  profileDescription.textContent = profileDescriptionEdit.value;
+  event.preventDefault();
 };
 
-editButton.addEventListener('click', function(event) {
-    togglePopup();
-    profileNameEdit.value = profileName.textContent;
-    profileDescriptionEdit.value = profileDescription.textContent;
-    event.preventDefault();
+/* открытие попапа для редактирования информации профиля */
+editButton.addEventListener("click", function (evt) {
+  togglePopup(popupInformacion);
+  profileNameEdit.value = profileName.textContent;
+  profileDescriptionEdit.value = profileDescription.textContent;
+  evt.preventDefault();
 });
 
-exitButton.addEventListener('click', function(event) {
-    togglePopup();
+exitButtonInfo.addEventListener("click", function (evt) {
+  togglePopup(popupInformacion);
 });
 
-popupForm.addEventListener('submit', formSubmitHandler);
-submitButton.addEventListener('click', formSubmitHandler);
+popupFormInfo.addEventListener("submit", formSubmitHandlerInfo);
+submitButtonInfo.addEventListener("click", formSubmitHandlerInfo);
 
-popup.addEventListener('click', function(event) {
-    if(event.target == event.currentTarget){
-        popup.classList.remove('popup_opened');
-        page.classList.remove('page_active');
-    }
+/* убирает попап для редактирования профиля при клике на области кроме окна попапа */
+popupInformacion.addEventListener("click", function (evt) {
+  if (evt.target == evt.currentTarget) {
+    popupInformacion.classList.remove("popup_opened");
+    page.classList.remove("page_active");
+  }
+});
+
+/* убирает попап для добавления изображений при клике на области кроме окна попапа */
+popupImages.addEventListener("click", function (evt) {
+  if (evt.target == evt.currentTarget) {
+    popupImages.classList.remove("popup_opened");
+    page.classList.remove("page_active");
+  }
+});
+
+/* открытие попапа для загрузки изображений */
+addImageButton.addEventListener("click", function (evt) {
+  togglePopup(popupImages);
+  evt.preventDefault();
+});
+
+/* закрытие попапа для загрузки изображений через крестик*/
+exitButtonImages.addEventListener("click", function (evt) {
+  togglePopup(popupImages);
+});
+
+/* функция добавления лайков на картинки */
+const addLikeImage = (evt) => {
+  evt.target.classList.toggle("element__like_active");
+};
+
+/* функция удаления картинки нажатием на корзину */
+const deleteImage = (evt) => {
+  evt.target.closest(".element").remove();
+};
+
+const imageTemplate = document.querySelector("#image-template").content;
+
+
+/* функция добавления новой картинки, установка лайка и удаления */
+const addImage = (imageValue, titleValue) => {
+  const imageElement = imageTemplate.querySelector(".element").cloneNode(true);
+  imageElement.querySelector(".element__image").src = imageValue;
+  imageElement.querySelector(".element__text").textContent = titleValue;
+  elements.prepend(imageElement);
+
+  const likeImageButton = imageElement.querySelector(".element__like");
+  likeImageButton.addEventListener("click", addLikeImage);
+
+  const deleteImageButton = imageElement.querySelector(".element__remove");
+  deleteImageButton.addEventListener("click", deleteImage);
+};
+
+/* функция внесения информации с попапа инфо в блок профиля */
+const formSubmitHandlerImages = () => {
+  addImage(elementsImageEdit.value, elementsTextEdit.value);
+  event.preventDefault();
+  togglePopup(popupImages);
+  elementsTextEdit.value = "";
+  elementsImageEdit.value = "";
+};
+
+popupFormImages.addEventListener("submit", formSubmitHandlerImages);
+submitButtonImages.addEventListener("click", formSubmitHandlerImages);
+
+/* Шесть карточек «из коробки» */
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function (element) {
+  addImage(element.link, element.name);
 });
