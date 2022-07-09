c7056534d0225cee1775f3b58fd9de14c7722d0f
DimDimShishkov <99988422+DimDimShishkov@users.noreply.github.com>
2022-07-09 15:16:17 +0300

    отладка функции удаления карточки с сервера

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -1,14 +1,39 @@
 import { Popup } from "./Popup";
 
-export class PopupWithConfirmation extends Popup {
-  constructor(popupSelector) {
+export default class PopupWithConfirmation extends Popup {
+  constructor(popupSelector, { submitHandler }) {
     super(popupSelector);
-    this._submitButton = this._popup.querySelector('.popup__submit-button')
+    this._submitButton = this._popup.querySelector(".popup__submit-button");
+    this._submitHandler = submitHandler;
   }
-// тестовый вариант 
+
+  // уведомление о процессе загрузки
+  renderLoading(isLoading, loadingMessage = 'Удаление...') {
+    if (isLoading) {
+      this._submitButton.textContent = loadingMessage
+    } else {
+      this._submitButton.textContent = this._initialTextSubmitButton;
+    }
+  }
+
+/*   _submitEvtHandler(evt) {
+    evt.preventDefault();
+    this._submitHandler();
+  }
+
+//  Метод setEventListeners с добавленным обработчиком сабмита формы
+setEventListeners() {
+  this._submitButton.addEventListener(
+    "submit",
+    this._submitEvtHandler.bind(this)
+  );
+  super.setEventListeners();
+} */
+
+  // тестовый вариант
   open(callback) {
-super.open();
-this.callback = callback;
-this._submitButton.onclick = this.callback
+    super.open();
+    this.callback = callback;
+    this._submitButton.onclick = this.callback;
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,7 @@ import "./index.css";
 import PopupWithImage from "../components/PopupWithImage.js";
 import { Section } from "../components/Section.js";
 import { Api } from "../components/Api.js";
+import PopupWithConfirmation from "../components/PopupWithConfirmation.js";
 
 // запрос к серверу
 const api = new Api(configApi);
@@ -43,8 +44,18 @@ api
 // создание попапа с вводом инфо в блок профиля
 const popupFormInfo = new PopupWithForm(".popup_type_info", {
   submitHandler: (data) => {
-    api.handleUploadProfileInfo(data);
-    userInfo.setUserInfo(data.title, data.description);
+    popupFormInfo.renderLoading(true);
+    api
+      .handleUploadProfileInfo(data)
+      .then((res) => {
+        userInfo.setUserInfo(res.name, res.about);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        popupFormInfo.renderLoading(false);
+      });
   },
 });
 
@@ -58,31 +69,34 @@ editButton.addEventListener("click", () => {
   profileDescriptionEdit.value = userData.description;
   popupFormInfo.open();
   formValidators["info"].resetValidation();
-  api.handleDownloadProfileInfo().then((res) => {
-    console.log(res);
-  });
 });
 
 // создание попапа со сменой аватара профиля
 const popupFormAvatar = new PopupWithForm(".popup_type_avatar", {
   submitHandler: (data) => {
-    api.handleUploadProfileAvatar(data);
-    userInfo.setUserAvatar(data.avatar);
-    console.log(data.avatar);
+    popupFormAvatar.renderLoading(true);
+    api
+      .handleUploadProfileAvatar(data)
+      .then((res) => {
+        userInfo.setUserAvatar(res.avatar);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        popupFormAvatar.renderLoading(false);
+      });
   },
 });
+
+// установка слушателя на попап со сменой аватара профиля
+popupFormAvatar.setEventListeners();
 
 /* открытие попапа с загрузкой аватара профиля */
 profileAvatarEditButton.addEventListener("click", () => {
   popupFormAvatar.open();
   formValidators["new-avatar"].resetValidation();
-  api.handleDownloadProfileInfo().then((res) => {
-    console.log(res.avatar);
-  });
 });
-
-// установка слушателя на попап со сменой аватара профиля
-popupFormAvatar.setEventListeners();
 
 // функция создания новой карточки
 const handleCreateCard = (item) => {
@@ -98,11 +112,22 @@ api.handleDownloadCards().then((res) => {
   });
 });
 
+
 /* создание попапа для загрузки новой картинки */
 const popupFormImage = new PopupWithForm(".popup_type_images", {
   submitHandler: (item) => {
-    cardList.addItemPrepend(handleCreateCard(item));
-    api.handleUploadCard(item);
+    popupFormImage.renderLoading(true);
+    api
+      .handleUploadCard(item)
+      .then((res) => {
+        cardList.addItemPrepend(handleCreateCard(res));
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        popupFormImage.renderLoading(false);
+      });
   },
 });
 
@@ -115,8 +140,11 @@ addImageButton.addEventListener("click", () => {
   popupFormImage.open();
 });
 
+
 // всплывающее окно для просмотра картинок
 const popupWithImage = new PopupWithImage(".popup_type_image");
+
+// установка слушателя на попап для просмотра картинок
 popupWithImage.setEventListeners();
 
 /* открытие попапа для просмотра изображений */
@@ -124,13 +152,27 @@ const handleCardClick = (link, title) => {
   popupWithImage.open(link, title);
 };
 
+// открытие попапа для удаления картинки
+/* const handleCardDelete = (id, card) => {
+  PopupDeleteCard.open()
+} */
+
 // создание попапа для удаления картинки
-/* const popupDeleteImage = new PopupWithForm(".popup_type_delete-image", {
+const PopupDeleteCard = new PopupWithConfirmation(".popup_type_delete-image", {
   submitHandler: (item) => {
-    //  cardList.addItemPrepend(handleCreateCard(item));
-    api.handleDeleteServerCard(item);
+    PopupDeleteCard.renderLoading(true)
+    api.handleDeleteServerCard(item._id).then((res) => {
+      console.log(res)
+    }).catch((err) => {
+      console.log(err)
+    }).finally(() => {
+      PopupDeleteCard.renderLoading(false)
+    })
   },
-}); */
+});
+
+// установка слушателя на попап для удаления картинки
+PopupDeleteCard.setEventListeners()
 
 // определение секции, куда положить новые карточки
 const cardList = new Section(
